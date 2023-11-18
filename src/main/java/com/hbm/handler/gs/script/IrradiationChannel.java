6011f8e8dcbccc1a355d5571394e052a364ad593
yor42 <49832212+yor42@users.noreply.github.com>
2023-11-18 17:47:34 +0900

    make IrradiationChannel reloadable

--- src/main/java/com/hbm/handler/gs/script/IrradiationChannel.java
+++ src/main/java/com/hbm/handler/gs/script/IrradiationChannel.java
@@ -11,16 +11,29 @@ import com.hbm.util.Tuple;
 import net.minecraft.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Iterator;
+
 import static com.hbm.inventory.RBMKOutgasserRecipes.rbmkOutgasserRecipes;
 
-public class IrradiationChannel extends VirtualizedRegistry<Tuple.Triplet<Integer, IIngredient, ItemStack>> {
+public class IrradiationChannel extends VirtualizedRegistry<Tuple.Pair<RecipesCommon.ComparableStack, Object[]>> {
     @Override
     public void onReload() {
-        this.removeScripted().forEach(recipe->removeRecipe(recipe.getY()));
+        this.removeScripted().forEach(this::removeRecipe);
+        this.restoreFromBackup().forEach(this::addRecipe);
+    }
+
+    public void removeRecipe(Tuple.Pair<RecipesCommon.ComparableStack, Object[]> pair){
+        rbmkOutgasserRecipes.remove(pair.getKey(), pair.getValue());
+        this.addBackup(pair);
     }
 
     public void removeAll(){
-        rbmkOutgasserRecipes.clear();
+        for (Iterator<RecipesCommon.ComparableStack> it = rbmkOutgasserRecipes.keySet().iterator(); it.hasNext(); ) {
+            RecipesCommon.ComparableStack stack = it.next();
+            Object[] param = rbmkOutgasserRecipes.get(stack);
+            this.addBackup(new Tuple.Pair<>(stack, param));
+            it.remove();
+        }
     }
 
     public void removeRecipe(IIngredient ingredient){
@@ -30,7 +43,10 @@ public class IrradiationChannel extends VirtualizedRegistry<Tuple.Triplet<Intege
     }
 
     public void removeRecipe(ItemStack stack){
-        RBMKOutgasserRecipes.removeRecipe(stack);
+        RecipesCommon.ComparableStack comparableStack = new RecipesCommon.ComparableStack(stack);
+        Object[] param = rbmkOutgasserRecipes.get(comparableStack);
+        rbmkOutgasserRecipes.remove(comparableStack, param);
+        this.addBackup(new Tuple.Pair<>(comparableStack, param));
     }
 
     public void addRecipe(Tuple.Triplet<Integer, IIngredient, ItemStack> triplet){
@@ -44,7 +60,14 @@ public class IrradiationChannel extends VirtualizedRegistry<Tuple.Triplet<Intege
     }
 
     public void addRecipe(int flux, ItemStack input, ItemStack out){
-        RBMKOutgasserRecipes.addRecipe(flux, input, out);
+        RecipesCommon.ComparableStack stack = new RecipesCommon.ComparableStack(input);
+        Object[] objects = new Object[] {flux, out};
+        addRecipe(new Tuple.Pair<>(stack, objects));
+    }
+
+    public void addRecipe(Tuple.Pair<RecipesCommon.ComparableStack, Object[]> pair){
+        rbmkOutgasserRecipes.put(pair.getKey(), pair.getValue());
+        this.addScripted(pair);
     }
 
     public static class RecipeBuilder extends AbstractRecipeBuilder<Tuple.Triplet<Integer, IIngredient, ItemStack>> {
